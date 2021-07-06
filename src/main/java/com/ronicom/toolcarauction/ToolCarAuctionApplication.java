62ff06958c7b8befa4bfdf71117ac2d3df0681db
Romilio Corona <romilio.aibal@gmail.com>
2021-07-06 20:57:59 -0300

    Initial commit

---
+++ src/main/java/com/ronicom/toolcarauction/ToolCarAuctionApplication.java
@@ -0,0 +1,13 @@
+package com.ronicom.toolcarauction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolCarAuctionApplication {
+
+   public static void main(String[] args) {
+      SpringApplication.run(ToolCarAuctionApplication.class, args);
+   }
+
+}
---
+++ src/test/java/com/ronicom/toolcarauction/ToolcarAuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ronicom.toolcarauction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolCarAuctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
